0ae24789300ba1f07ab13923dbeff4f5951d0d41
Fränk Klein <frank@humanmade.co.uk>
2020-01-31 22:28:21 +0100

    Fix path to Bylines main plugin file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,7 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
-    require_once dirname( __DIR__ ) . 'byline.php';
+    require_once dirname( __DIR__ ) . '/byline.php';
 } );
 
 // Start up the WP testing environment.
