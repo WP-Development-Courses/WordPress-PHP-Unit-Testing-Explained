ef035110c79036630a4545582b0a877ec61a17be
Fränk Klein <frank@humanmade.co.uk>
2020-03-04 07:56:27 +0100

    Add exercise for creating a user fixture.

--- tests/test-exercise-fixtures-solution.php
+++ tests/test-exercise-fixtures-solution.php
@@ -20,4 +20,20 @@ class Test_Exercise_Assertions_Solution extends WP_UnitTestCase {
 		$this->assertEquals( 1, $count['total_comments'] );
 		$this->assertEquals( 0, $count['trash'] );
 	}
+
+	/**
+	 * Verify that searching for a part of a user's nicename returns the user.
+	 */
+	public function test_search_users_nicename() {
+		$user_id = self::factory()->user->create( [
+			'user_nicename' => 'maxmustermann',
+		] );
+
+		$users = get_users( [
+			'search' => '*muster*',
+			'fields' => 'ID',
+		] );
+
+		$this->assertTrue( in_array( $user_id, $users ) );
+	}
 }
