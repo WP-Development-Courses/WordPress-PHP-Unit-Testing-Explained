40168e821ee027b847f6701158647ad4103c2551
Fränk Klein <frank@humanmade.co.uk>
2020-05-19 16:15:41 +0200

    Remove Byline override test code to only leave starter code.

--- tests/test-byline-override.php
+++ tests/test-byline-override.php
@@ -8,13 +8,5 @@ class Test_Byline_Override extends WP_UnitTestCase {
 	}
 
 	function test_custom_byline_is_saved() {
-		$_POST['byline-override'] = $this->byline_override;
-
-		byline_save_override_meta_data( self::$post_id );
-
-		$this->assertSame(
-			$this->byline_override,
-			get_post_meta( self::$post_id, 'byline-override', true )
-		);
 	}
 }
