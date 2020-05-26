7760d2f1471045472f4e27068075f774c4398eaf
Fränk Klein <frank@humanmade.co.uk>
2020-05-26 14:42:56 +0200

    Revert "Add completed code for the Creating PHP Superglobal fixtures lesson."
    This reverts commit 25fe4d30fc1f75264a9e589727ccf1f5890daad2.

--- tests/test-byline-override.php
+++ tests/test-byline-override.php
@@ -3,20 +3,10 @@
 class Test_Byline_Override extends WP_UnitTestCase {
 	public static $post_id;
 
-	public $byline_override = 'Byline override';
-
 	public static function wpSetupBeforeClass() {
 		self::$post_id = self::factory()->post->create();
 	}
 
 	function test_custom_byline_is_saved() {
-		$_POST['byline-override'] = $this->byline_override;
-
-		byline_save_override_meta_data( self::$post_id );
-
-		$this->assertSame(
-			$this->byline_override,
-			get_post_meta( self::$post_id, 'byline-override', true )
-		);
 	}
 }
