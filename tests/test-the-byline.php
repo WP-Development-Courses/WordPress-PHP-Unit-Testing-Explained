6fb9d1a1b1e98188f45c6a793b76b882c5e08679
Fränk Klein <frank@humanmade.co.uk>
2020-05-28 08:40:34 +0200

    Add unit tests for the `the_byline` template tag.

---
+++ tests/test-the-byline.php
@@ -0,0 +1,67 @@
+<?php
+
+class The_Byline_Test extends WP_UnitTestCase {
+	public static $post_id;
+	public static $user_id;
+
+	public static function wpSetupBeforeClass() {
+		self::$user_id = self::factory()->user->create( [
+			'display_name' => 'John Doe',
+		] );
+
+		self::$post_id = self::factory()->post->create( [
+			'post_author' => self::$user_id,
+			'post_content' => '<!-- wp:paragraph -->
+<p>First</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:nextpage -->
+<!--nextpage-->
+<!-- /wp:nextpage -->
+
+<!-- wp:paragraph -->
+<p>Second</p>
+<!-- /wp:paragraph -->',
+		] );
+	}
+
+    function test_default_args_first_page() {
+       $this->go_to( get_permalink( self::$post_id ) );
+	    setup_postdata( get_post( self::$post_id ) );
+
+       $this->assertSame(
+       	    'Written by John Doe',
+	       get_echo( 'the_byline' )
+       );
+    }
+
+	function test_default_args_second_page() {
+		$this->go_to( get_permalink( self::$post_id ) . '&page=2' );
+		setup_postdata( get_post( self::$post_id ) );
+
+		$this->assertSame(
+			'Written by John Doe',
+			get_echo( 'the_byline' )
+		);
+	}
+
+	function test_hide_on_paged_first_page() {
+		$this->go_to( get_permalink( self::$post_id ) );
+		setup_postdata( get_post( self::$post_id ) );
+
+		$this->assertSame(
+			'Written by John Doe',
+			get_echo( 'the_byline', [ [ 'hide_on_paged' => true ] ] )
+		);
+	}
+
+	function test_hide_on_paged_second_page() {
+		$this->go_to( get_permalink( self::$post_id ) . '&page=2' );
+		setup_postdata( get_post( self::$post_id ) );
+
+		$this->assertSame(
+			'',
+			get_echo( 'the_byline', [ [ 'hide_on_paged' => true ] ] )
+		);
+	}
+}
