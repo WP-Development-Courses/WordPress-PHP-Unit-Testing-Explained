0c98c5f43e113f5101c1eb1ff1a4e4319d791436
Fränk Klein <frank@humanmade.co.uk>
2020-07-05 10:00:07 +0200

    Enable pretty permalinks.

--- tests/test-exercise-solution.php
+++ tests/test-exercise-solution.php
@@ -1,11 +1,22 @@
 <?php
 
 class Test_Exercise_Solution extends WP_UnitTestCase {
-    function test_category_page_body_class() {
+	function setUp() {
+		parent::setUp();
+
+		// Enable pretty permalinks.
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' );
+		// See https://core.trac.wordpress.org/ticket/35452
+		create_initial_taxonomies();
+	}
+
+	function test_category_page_body_class() {
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' );
+
 	    $category_id = self::factory()->category->create( array( 'name' => 'foo' ) );
 	    self::factory()->post->create( array( 'category' => $category_id ) );
 
-	    $this->go_to( home_url( "?cat=$category_id" ) );
+	    $this->go_to( home_url( '/category/foo/' ) );
 
 	    $this->assertContains( "category-$category_id", get_body_class() );
     }
