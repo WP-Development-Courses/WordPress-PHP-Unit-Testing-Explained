3a4869a489763edea8c0e4ad5c250cceb7e1e8b7
Fränk Klein <frank@humanmade.co.uk>
2020-11-02 14:34:49 +0100

    Update install script to fix test database creation SQL errors:
    - Use `sudo` when creating the database.
    - Set a `wp` user for the test database.

--- install.sh
+++ install.sh
@@ -4,10 +4,17 @@
 set -e
 
 # Install Composer dependencies.
+echo -e "Step 1: Install Composer dependencies"
 composer install --no-suggest
 
 # Create the test database.
-mysqladmin create byline_tests -u root -proot
+echo -e "Step 2: Create the test database"
+echo -e " * Creating database 'byline_tests' (if it's not already there)"
+sudo mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`byline_tests\`"
+echo -e " * Granting the wp user priviledges to the 'byline_tests' database"
+sudo mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`byline_tests\`.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e " * DB operations done."
 
 # Do a test run.
+echo -e "Step 3: Do a test run"
 composer test
